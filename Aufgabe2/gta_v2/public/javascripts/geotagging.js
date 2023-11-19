8fb6054b60b0938af104e0fc582b57701257af06
Luca Rosato <luca.rosato@outlook.de>
2023-11-19 12:33:19 +0100

    Aufgabe 2 Commenting

--- Aufgabe2/gta_v2/public/javascripts/geotagging.js
+++ Aufgabe2/gta_v2/public/javascripts/geotagging.js
@@ -111,21 +111,21 @@ class MapManager {
  * It is called once the page has been fully loaded.
  */
 function updateLocation() {
-  LocationHelper.findLocation(function (helper) {
-    var latitude = helper.latitude;
-    var longitude = helper.longitude;
-    document.getElementById("latitude_input").value = latitude;
-    document.getElementById("longitude_input").value = longitude;
-    document.getElementById("latitude_hidden_input").value = latitude;
-    document.getElementById("longitude_hidden_input").value = longitude;
-    var img = document.getElementById("mapView");
-    let mapmanager = new MapManager("wlvNEFoQ5OCICOpOR62Y4VzcsPDWcZJJ");
-    var mapQuestUrl = mapmanager.getMapUrl(latitude, longitude);
-    img.src = mapQuestUrl;
-  });
+  LocationHelper.findLocation(function (helper) { //findLocation return LocationHelper object helper to callback function
+    var latitude = helper.latitude; //saves latitude from helper into var latitude 
+    var longitude = helper.longitude; //saves longitude from helper into var longitude 
+    document.getElementById("latitude_input").value = latitude; //pass latitude value into latitude input on website
+    document.getElementById("longitude_input").value = longitude; //pass longitude value into longitude input on website
+    document.getElementById("latitude_hidden_input").value = latitude; //same on hidden input in discovery search
+    document.getElementById("longitude_hidden_input").value = longitude;  //same on hidden input in discovery search
+    var img = document.getElementById("mapView"); //create var for map img
+    let mapmanager = new MapManager("wlvNEFoQ5OCICOpOR62Y4VzcsPDWcZJJ"); // create MapManager object with custom API key
+    var mapQuestUrl = mapmanager.getMapUrl(latitude, longitude); //call getMapUrl() with latitude and longitude as arguments
+    img.src = mapQuestUrl; //change map img source to MapQuest URL
+  }); // findLocation()-call with callback-function as argument: function(helper){...}
 }
 
 // Wait for the page to fully load its DOM content, then call updateLocation
 document.addEventListener("DOMContentLoaded", () => {
-  updateLocation();
+  updateLocation(); // call function above
 });
