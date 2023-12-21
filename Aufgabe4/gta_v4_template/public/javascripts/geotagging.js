6efc1770d8106421658de5f440e62fcb14806c2b
Luca Rosato <luca.rosato@outlook.de>
2023-12-21 20:57:02 +0000

    geotagging fetch functions fully commented

--- Aufgabe4/gta_v4_template/public/javascripts/geotagging.js
+++ Aufgabe4/gta_v4_template/public/javascripts/geotagging.js
@@ -3,81 +3,114 @@
 /* eslint-disable no-unused-vars */
 
 console.log("The geoTagging script is going to start...");
-/**
- *
- *
- * @return {*} 
- */
+
 function updateLocation() {
-  var img = document.getElementById("mapView"); //create var for map img
-  let mapmanager = new MapManager("wlvNEFoQ5OCICOpOR62Y4VzcsPDWcZJJ"); // create MapManager object with custom API key
+  //Create var for map img
+  var img = document.getElementById("mapView");
+
+  //Mapmanager Object with Mapquest API key
+  let mapmanager = new MapManager("wlvNEFoQ5OCICOpOR62Y4VzcsPDWcZJJ");
+
   var mapQuestUrl;
   var latitudeValue = document.getElementById("latitude_input").value;
   var longitudeValue = document.getElementById("longitude_input").value;
 
-  const taglist = JSON.parse(img.dataset.tags); //Parse list from img data tag to JSON
-  console.log(taglist);
+  //Parse list from img data tag to JSON
+  const taglist = JSON.parse(img.dataset.tags);
 
+  //Latitude and longitude exist
   if (latitudeValue && longitudeValue) {
-    //when there are already values for lat and lon
+    console.log("-".repeat(43));
     console.log("Coordinates already exist:", latitudeValue, longitudeValue);
-    mapQuestUrl = mapmanager.getMapUrl(latitudeValue, longitudeValue, taglist); //call getMapUrl() with latitude and longitude as arguments
-    img.src = mapQuestUrl; //change map img source to MapQuest URL
+    console.log("-".repeat(43));
+
+    //Call getMapUrl() with latitude and longitude as arguments
+    mapQuestUrl = mapmanager.getMapUrl(latitudeValue, longitudeValue, taglist);
+
+    //Change map img source to MapQuest URL
+    img.src = mapQuestUrl;
+
     return;
   }
 
+  console.log("-".repeat(25));
   console.log("findLocation will be executed");
+  console.log("-".repeat(25));
+
   LocationHelper.findLocation(function (helper) {
-    //findLocation return LocationHelper object helper to callback function
-    var latitude = helper.latitude; //saves latitude from helper into var latitude
-    var longitude = helper.longitude; //saves longitude from helper into var longitude
+    //Saves latitude & longitude from helper
+    var latitude = helper.latitude;
+    var longitude = helper.longitude;
 
-    document.getElementById("latitude_input").value = latitude; //pass latitude value into latitude input on website
-    document.getElementById("longitude_input").value = longitude; //pass longitude value into longitude input on website
-    document.getElementById("latitude_hidden_input").value = latitude; //same on hidden input in discovery search
-    document.getElementById("longitude_hidden_input").value = longitude; //same on hidden input in discovery search
+    //Pass values on elmt values
+    document.getElementById("latitude_input").value = latitude;
+    document.getElementById("longitude_input").value = longitude;
+    document.getElementById("latitude_hidden_input").value = latitude;
+    document.getElementById("longitude_hidden_input").value = longitude;
 
-    mapQuestUrl = mapmanager.getMapUrl(latitude, longitude, taglist); //call getMapUrl() with latitude and longitude as arguments
-    img.src = mapQuestUrl; //change map img source to MapQuest URL
-  }); // findLocation()-call with callback-function as argument: function(helper){...}
+    mapQuestUrl = mapmanager.getMapUrl(latitude, longitude, taglist);
+
+    //Change map img source to MapQuest URL
+    img.src = mapQuestUrl;
+  });
 }
 
 function taggingFormEventListener() {
+  //Declaring variables
   const taggingForm = document.getElementById("tag-form");
 
+  //EventListener for tagging from ON SUBMIT
   taggingForm.addEventListener("submit", (event) => {
+    //Prevent default behavior
     event.preventDefault();
 
-    const data = new FormData(taggingForm); //4 arrays with key/value pair lat lon hashtag name
-    //use data from FormData and paste it into js object which will be later on stringified to JSON
-    const dataAsJSON = {};
-    data.forEach((value, key) => {
-      dataAsJSON[key] = value;
-    });
+    //Create formData
+    const formData = new FormData(taggingForm);
+    //Convert formData to JS Object
+    const data = Object.fromEntries(formData);
 
+    //logging
+    console.log("-".repeat(100));
+    console.log(
+      "FETCH TAGGING POST REQUEST TRIGGERED:\n" +
+        "data: " +
+        JSON.stringify(data)
+    );
+    console.log("-".repeat(100));
+
+    //Fetch Request for adding Geotags
     fetch("/api/geotags", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(dataAsJSON),
+      body: JSON.stringify(data),
     })
       .then((res) => res.json())
       .then((data) => {
-        const tagListElement = document.getElementById("discoveryResults");
+        //Variable for unordered list in discoveryResults
+        const taglistHTML = document.getElementById("discoveryResults");
 
+        //Extract variables from data (order does not matter)
         const { name, latitude, longitude, hashtag } = data;
+
+        //Create listElmt
         const listItem = document.createElement("li");
+
+        //Fill with content "name (lat,lon) hashtag"
         listItem.textContent = `${name} (${latitude},${longitude}) ${hashtag}`;
-        tagListElement.appendChild(listItem);
+        taglistHTML.appendChild(listItem);
       });
   });
 }
 
 function discoveryFormEventListener() {
+  //Declaring vars
   const discoveryForm = document.getElementById("discoveryFilterForm");
 
+  //EventListener for discovery from ON SUBMIT
   discoveryForm.addEventListener("submit", (event) => {
+    //Prevent default behavior
     event.preventDefault();
 
     const searchterm = document.getElementById("searchterm_input").value;
@@ -90,6 +123,7 @@ function discoveryFormEventListener() {
       searchterm: searchterm,
     });
 
+    //fetch "/api/geotags?" with query params e.g "name='luca'&age='19'
     fetch("/api/geotags?" + queryParams.toString(), {
       method: "GET",
       headers: {
@@ -98,10 +132,21 @@ function discoveryFormEventListener() {
     })
       .then((res) => res.json())
       .then((data) => {
-        // Erhaltene Daten in die taglist im EJS-Template einfügen
         const tagListElement = document.getElementById("discoveryResults");
-        tagListElement.innerHTML = ""; // Leeren der aktuellen Listenelemente
 
+        //logging
+        console.log("-".repeat(100));
+        console.log(
+          "FETCH DISCOVERY GET REQUEST TRIGGERED:\n" +
+            "data: " +
+            JSON.stringify(data)
+        );
+        console.log("-".repeat(100));
+
+        //Empty current fill
+        tagListElement.innerHTML = "";
+
+        //Iterate through and append to list
         data.taglist.forEach((gtag) => {
           const listItem = document.createElement("li");
           listItem.textContent = `${gtag.name} (${gtag.latitude},${gtag.longitude}) ${gtag.hashtag}`;
@@ -118,6 +163,6 @@ function ajaxFetch() {
 
 // Wait for the page to fully load its DOM content, then call updateLocation
 document.addEventListener("DOMContentLoaded", () => {
-  updateLocation(); // call function above
+  updateLocation();
   ajaxFetch();
 });
